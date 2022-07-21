ebcc0842274ed86e8eabb3bbd3d785dd2df268ee
Hussain-Lohawala <hussainlohawala33@gmail.com>
2022-07-21 23:15:02 +0530

    Updated reset button and added Heading.

--- work.js
+++ work.js
@@ -1,10 +1,11 @@
 var greenBallCount =0;
 function generateGreen(){
-    console.log("Hello World");
+    console.log("Game Started");
     var node = document.createElement("div"); 
     node.setAttribute("class", "ball greenball"); 
     document.getElementsByClassName("ballContainer")[0].appendChild(node);
     greenBallCount+=1;
+    document.getElementById('resetButton').innerHTML="RESET";
 }
 function removeBall(){
     if(greenBallCount>0){
@@ -12,4 +13,11 @@ function removeBall(){
         greenBallCount-=1;
     }
 
+}
+function reset(){
+    while(greenBallCount){
+        document.getElementsByClassName("greenball")[0].remove();
+        greenBallCount-=1;
+    }
+    document.getElementById('resetButton').innerHTML="START";
 }
